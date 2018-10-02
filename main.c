e6e0f41f01048043ce19dbdb4721c906c5172dc3
Charles Manning <cdhmanning@gmail.com>
2018-10-03 08:19:23 +1300

    Add a few more files & tweak main and notes
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include "stm32f10x_rcc.h"
 
 #define GPIO_BANK	GPIOC
-#define GPIO_PIN	GPIO_Pin_13
+#define GPIO_PIN13	GPIO_Pin_13
+#define GPIO_PIN14	GPIO_Pin_14
+
 
 /* timing is not guaranteed :) */
 void simple_delay(uint32_t us)
@@ -30,47 +32,110 @@ void  SysTick_Handler(void)
     flash_count = 0;
     
     if (gpio_on)
-        GPIO_SetBits(GPIO_BANK, GPIO_PIN);
+        GPIO_SetBits(GPIO_BANK, GPIO_PIN13);
     else
-        GPIO_ResetBits(GPIO_BANK, GPIO_PIN);
+        GPIO_ResetBits(GPIO_BANK, GPIO_PIN13);
     gpio_on = !gpio_on;
         
 }
 
 int main(void)
 {
-
     GPIO_InitTypeDef gpio;
  
-    
-    SysTick_Config(72000000/1000);
-    
     /* enable gpio clocks */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
  
     GPIO_StructInit(&gpio);
-    gpio.GPIO_Pin = (GPIO_PIN);
+    gpio.GPIO_Pin = (GPIO_PIN13);
     gpio.GPIO_Mode = GPIO_Mode_Out_PP;
-    gpio.GPIO_Speed =  GPIO_Speed_10MHz;
+    gpio.GPIO_Speed =  GPIO_Speed_50MHz;
     GPIO_Init(GPIO_BANK, &gpio);
-    
-    __enable_irq();
+
+    GPIO_StructInit(&gpio);
+    gpio.GPIO_Pin = (GPIO_PIN14);
+    gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+    gpio.GPIO_Speed =  GPIO_Speed_50MHz;
+    GPIO_Init(GPIO_BANK, &gpio);
  
- #if 0
+ #if 1
     /* main program loop */
-    for (;;) {
-        /* set led on */
-        GPIO_SetBits(GPIO_BANK, GPIO_PIN);
+    while(1) {
+        /* Make 2 sqaure waves 90 degrees out of phase. */
+
+#if 0
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+#else
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+
+        GPIO_BANK->BSRR = GPIO_PIN13;
+        GPIO_BANK->BSRR = GPIO_PIN13;
+//        GPIO_BANK->BSRR = GPIO_PIN14;
+//        GPIO_BANK->BSRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN13;
+        GPIO_BANK->BRR = GPIO_PIN14;
+        GPIO_BANK->BRR = GPIO_PIN14;
+#endif
         /* delay */
-        simple_delay(100000);
+        //simple_delay(100000);
         /* clear led */
-        GPIO_ResetBits(GPIO_BANK, GPIO_PIN);
+        //GPIO_ResetBits(GPIO_BANK, GPIO_PIN);
         /* delay */
-        simple_delay(100000);
+        //simple_delay(100000);
     }
  
  #else
+     SysTick_Config(72000000/1000);
+    
+     __enable_irq();
+
      while(1) {
          __WFI();
     }
